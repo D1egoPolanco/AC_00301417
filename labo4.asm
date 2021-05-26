c6b3a4932a464db292d37a5ae88686261a7f9a6e
Diego Polanco <37520507+D1egoPolanco@users.noreply.github.com>
2021-05-25 22:23:37 -0600

    labo 4

---
+++ labo4.asm
@@ -0,0 +1,54 @@
+section .text
+
+XOR SI, SI                          
+XOR DI, DI
+XOR BX, BX
+XOR CX, CX
+XOR DX, DX
+XOR AX, AX
+
+MOV     BP, arreglo     
+CALL    datos        
+
+int     20h
+
+section .data
+
+arreglo db 11,22,33,44,55,66,77,88,99,98 ;datos arreglo   
+
+largo equ $-array ;tamano
+   
+dividir equ 02 ;
+
+datos:
+    MOV     BL, dividir
+
+for:
+    CMP     SI, largo
+    JE      end
+
+    MOV     AL, [BP+SI]
+    MOV     BH,AL
+    DIV     BL
+    INC     SI
+
+    CMP     AH, 0
+    JE      es_par
+    JNE     es_impar
+        
+par:
+    MOV     DI, CX
+    MOV     [300h+ DI], BH
+    INC     DI
+    MOV     CX, DI
+    jmp     for
+
+impar:
+    MOV     DI, DX
+    MOV     [320h+DI], BH
+    INC     DI
+    MOV     DX, DI
+    jmp     for
+
+end:
+    ret
